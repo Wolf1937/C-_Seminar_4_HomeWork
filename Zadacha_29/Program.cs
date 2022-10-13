421c3e25fc0aa89203c6b7a91fe47b54568a2575
Krutinis <krutinis@yandex.ru>
2022-10-13 10:20:03 +0300

    add Zadacha_29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] arr = new int[8];
+FillArray(arr.Length);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+void FillArray (int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = new Random().Next(100);
+    }
+}
