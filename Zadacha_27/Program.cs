28e0f35a31f7245cf1b4572e4a62b1c1e280c876
Krutinis <krutinis@yandex.ru>
2022-10-13 09:56:58 +0300

    add Zadacha_27

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int result = 0;
+Console.WriteLine($"Сумма цифр числа {number} -> {SumOfDigits(result)}.");
+
+
+int SumOfDigits (int size) {
+    int digit = 0;
+    for (int i = 1; number > 0; i++) {
+        digit = number % 10;
+        result = result + digit;
+        number /= 10;
+    }
+    return result;
+}
